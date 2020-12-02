f2f4260aaf52a22ea26255bb883667c4b4dcb66f
Mikhail.Kaduchka <mikhail.kaducha@miro.com>
2020-12-02 14:01:23 +0100

    Init commit

---
+++ src/main/java/com/miro/hackathon/augmentedreality/AugmentedRealityApplication.java
@@ -0,0 +1,13 @@
+package com.miro.hackathon.augmentedreality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugmentedRealityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AugmentedRealityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miro/hackathon/augmentedreality/AugmentedRealityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.hackathon.augmentedreality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugmentedRealityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
