fd765191f2084217d765bc4d3f5e7dd6a709e9d9
Mikhail.Kaduchka <mikhail.kaducha@miro.com>
2020-12-02 17:46:57 +0100

    Draft of making smart cutting for an image.

---
+++ src/main/java/com/miro/hackathon/augmentedreality/utility/ImageUtils.java
@@ -0,0 +1,43 @@
+package com.miro.hackathon.augmentedreality.utility;
+
+import java.awt.*;
+import java.awt.image.*;
+
+public class ImageUtils {
+
+    public static BufferedImage createResizedCopy(Image originalImage, int scaledWidth, int scaledHeight, boolean preserveAlpha) {
+        int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, imageType);
+        Graphics2D g = scaledBI.createGraphics();
+        if (preserveAlpha) {
+            g.setComposite(AlphaComposite.Src);
+        }
+        g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);
+        g.dispose();
+        return scaledBI;
+    }
+
+    public static Image transformGrayToTransparency(BufferedImage image) {
+        ImageFilter filter = new RGBImageFilter() {
+            public final int filterRGB(int x, int y, int rgb) {
+                return (rgb << 8) & 0xFF000000;
+            }
+        };
+
+        ImageProducer ip = new FilteredImageSource(image.getSource(), filter);
+        return Toolkit.getDefaultToolkit().createImage(ip);
+    }
+
+    public static BufferedImage applyTransparency(BufferedImage image, Image mask) {
+        BufferedImage dest = new BufferedImage(
+                image.getWidth(), image.getHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = dest.createGraphics();
+        g2.drawImage(image, 0, 0, null);
+        AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.DST_IN, 1.0F);
+        g2.setComposite(ac);
+        g2.drawImage(mask, 0, 0, null);
+        g2.dispose();
+        return dest;
+    }
+}
