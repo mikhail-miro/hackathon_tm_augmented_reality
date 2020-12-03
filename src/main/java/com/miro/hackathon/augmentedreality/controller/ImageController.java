22609bc0c40b459932234bcaf8bd1ed829f90fb2
Mikhail.Kaduchka <mikhail.kaducha@miro.com>
2020-12-03 10:44:55 +0100

    Wrap unprocessed images into Json object

--- src/main/java/com/miro/hackathon/augmentedreality/controller/ImageController.java
+++ src/main/java/com/miro/hackathon/augmentedreality/controller/ImageController.java
@@ -1,20 +1,13 @@
 package com.miro.hackathon.augmentedreality.controller;
 
-import com.miro.hackathon.augmentedreality.entity.ImageEntity;
+import com.miro.hackathon.augmentedreality.controller.response.UnprocessedResponse;
 import com.miro.hackathon.augmentedreality.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/images")
@@ -24,8 +17,8 @@ public class ImageController {
     private final ImageService imageService;
 
     @GetMapping("/unprocessed")
-    public List<ImageEntity> findUnprocessedImages(@RequestParam("boardId") String boardId) {
-        return imageService.findAllUnprocessedImagesByBoard(boardId);
+    public UnprocessedResponse findUnprocessedImages(@RequestParam("boardId") String boardId) {
+        return new UnprocessedResponse(imageService.findAllUnprocessedImagesByBoard(boardId));
     }
 
 
